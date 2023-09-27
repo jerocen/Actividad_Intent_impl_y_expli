452f588b0a857583115ca93a6e3838606cc3f4ed
jerocen <jeronimo17917@gmail.com>
2023-09-26 23:04:49 -0600

    Initial commit

---
+++ app/src/main/java/com/example/actividad_intent_impl_y_expli/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.actividad_intent_impl_y_expli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button2);
+    }
+
+    public void entrar(view v)
+    {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
