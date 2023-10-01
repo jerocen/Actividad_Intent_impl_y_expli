0546c49821a3fb8525b4a03bba04054faeda30e1
jerocen <jeronimo17917@gmail.com>
2023-09-30 20:56:43 -0600

    Archivos modificados del ejercicios de los Intent agregando los botones, proxima actualización se acomodarán y se les asignará su respectivo intent.

--- app/src/main/java/com/example/actividad_intent_impl_y_expli/MainActivity.java
+++ app/src/main/java/com/example/actividad_intent_impl_y_expli/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.View;
 import android.widget.Button;
 
 
@@ -19,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button2);
     }
 
-    public void entrar(view v)
+    public void entrar(View v)
     {
         Intent intent = new Intent(this, MainActivity2.class);
         startActivity(intent);
