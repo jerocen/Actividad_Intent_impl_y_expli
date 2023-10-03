32fa85f1a31639232770f1b10cce06b75fbd89b0
jerocen <jeronimo17917@gmail.com>
2023-10-02 20:29:44 -0600

    Archivos modificados del ejercicios además de archivos nuevos con respecto a las activities a donde dirigen los botones.

--- app/src/main/java/com/example/actividad_intent_impl_y_expli/MainActivity.java
+++ app/src/main/java/com/example/actividad_intent_impl_y_expli/MainActivity.java
@@ -2,27 +2,60 @@ package com.example.actividad_intent_impl_y_expli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.view.View;
 import android.widget.Button;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     Button button;
+
+    private TextView countdownTextView;
+    private CountDownTimer countDownTimer;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button2);
+        //button = findViewById(R.id.button2);
+
+        countdownTextView = findViewById(R.id.countdown_timer);
+
+        startCountdownTimer();
     }
+
+    private void startCountdownTimer()
+    {
+        countDownTimer = new CountDownTimer(5000, 1000)
+        {
+            @Override
+            public void onTick(long millisUntilFinished)
+            {
+                long secondRemaining = millisUntilFinished / 1000;
+                countdownTextView.setText(secondRemaining + "seg.");
+            }
+
+            @Override
+            public void onFinish()
+            {
+                Intent intent = new Intent(this, MainActivity2.class);
+                startActivity(intent);
+            }
+        }.start();
+    }
+
+    //-------------------------------------------------------------------------
 
     public void entrar(View v)
     {
-        Intent intent = new Intent(this, MainActivity2.class);
-        startActivity(intent);
+        //Intent intent = new Intent(this, MainActivity2.class);
+        //startActivity(intent);
     }
+
 }
